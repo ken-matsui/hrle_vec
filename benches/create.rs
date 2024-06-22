4210671d7925b322aae3c750c19e50bbcf12ea95
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-06-22 03:53:31 -0700

    added some benches

---
+++ benches/create.rs
@@ -0,0 +1,40 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use hrle_vec::HrleVec;
+
+fn bench_10_000_unique_values_from_slice(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Create 10_000 unique values from slice");
+
+    let vec = Vec::from_iter(0..10_000);
+    let slice = vec.as_slice();
+
+    group.bench_with_input(BenchmarkId::new("Hrle", ""), slice, |b, slice| {
+        b.iter(|| HrleVec::from(&slice[..]))
+    });
+    group.bench_with_input(BenchmarkId::new("Vec", ""), slice, |b, slice| {
+        b.iter(|| Vec::from(&slice[..]))
+    });
+
+    group.finish();
+}
+
+fn bench_10_000_unique_values_from_iter(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Create 10_000 unique values from iter");
+
+    let iter = (0..10_000).into_iter();
+
+    group.bench_with_input(BenchmarkId::new("Hrle", ""), &iter, |b, iter| {
+        b.iter(|| HrleVec::from_iter(iter.clone()))
+    });
+    group.bench_with_input(BenchmarkId::new("Vec", ""), &iter, |b, iter| {
+        b.iter(|| Vec::from_iter(iter.clone()))
+    });
+
+    group.finish();
+}
+
+criterion_group!(
+    benches,
+    bench_10_000_unique_values_from_slice,
+    bench_10_000_unique_values_from_iter
+);
+criterion_main!(benches);
