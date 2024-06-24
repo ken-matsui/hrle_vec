0703552827412d2c782301c989696027b50b8a26
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-06-23 17:40:19 -0700

    more create benches

--- benches/create.rs
+++ benches/create.rs
@@ -1,28 +1,131 @@
+use std::hint::black_box;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 use hrle_vec::HrleVec;
+use itertools::repeat_n;
+use rle_vec::RleVec;
 
-fn bench_10_000_unique_values_from_slice(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Create 10_000 unique values from slice");
+fn create_10_000_unique_values_from_slice(c: &mut Criterion) {
+    let mut group = c.benchmark_group("10,000 unique values from slice");
 
     let vec = Vec::from_iter(0..10_000);
     let slice = vec.as_slice();
 
-    group.bench_with_input("Hrle", slice, |b, slice| b.iter(|| HrleVec::from(slice)));
-    group.bench_with_input("Vec", slice, |b, slice| b.iter(|| Vec::from(slice)));
+    group.bench_with_input("Hrle", slice, |b, slice| {
+        b.iter(|| HrleVec::from(black_box(slice)))
+    });
+    group.bench_with_input("Rle", slice, |b, slice| {
+        b.iter(|| RleVec::from(black_box(slice)))
+    });
+    group.bench_with_input("Vec", slice, |b, slice| {
+        b.iter(|| Vec::from(black_box(slice)))
+    });
 
     group.finish();
 }
 
-fn bench_10_000_unique_values_from_iter(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Create 10_000 unique values from iter");
+fn create_10_000_unique_values_from_iter(c: &mut Criterion) {
+    let mut group = c.benchmark_group("10,000 unique values from iter");
 
-    let iter = 0..10_000;
+    group.bench_function("Hrle", |b| b.iter(|| HrleVec::from_iter(0..10_000)));
+    group.bench_function("Rle", |b| b.iter(|| RleVec::from_iter(0..10_000)));
+    group.bench_function("Vec", |b| b.iter(|| Vec::from_iter(0..10_000)));
 
-    group.bench_with_input("Hrle", &iter, |b, iter| {
-        b.iter(|| HrleVec::from_iter(iter.clone()))
+    group.finish();
+}
+
+fn create_10_000_equal_values_from_slice(c: &mut Criterion) {
+    let mut group = c.benchmark_group("10,000 equal values from slice");
+
+    let vec = vec![0; 10_000];
+    let slice = vec.as_slice();
+
+    group.bench_with_input("Hrle", slice, |b, slice| {
+        b.iter(|| HrleVec::from(black_box(slice)))
     });
-    group.bench_with_input("Vec", &iter, |b, iter| {
-        b.iter(|| Vec::from_iter(iter.clone()))
+    group.bench_with_input("Rle", slice, |b, slice| {
+        b.iter(|| RleVec::from(black_box(slice)))
+    });
+    group.bench_with_input("Vec", slice, |b, slice| {
+        b.iter(|| Vec::from(black_box(slice)))
+    });
+
+    group.finish();
+}
+
+fn create_10_000_equal_values_from_iter(c: &mut Criterion) {
+    let mut group = c.benchmark_group("10,000 equal values from iter");
+
+    group.bench_function("Hrle", |b| {
+        b.iter(|| HrleVec::from_iter(repeat_n(0, 10_000)))
+    });
+    group.bench_function("Rle", |b| b.iter(|| RleVec::from_iter(repeat_n(0, 10_000))));
+    group.bench_function("Vec", |b| b.iter(|| Vec::from_iter(repeat_n(0, 10_000))));
+
+    group.finish();
+}
+
+fn create_1000_runs_of_10_values_from_slice(c: &mut Criterion) {
+    let mut group = c.benchmark_group("1,000 runs of 10 values from slice");
+
+    let zeros = repeat_n(0, 10);
+    let ones = repeat_n(1, 10);
+    let iter = repeat_n(zeros.chain(ones), 10_000).flatten();
+
+    let vec = Vec::from_iter(iter);
+    let slice = vec.as_slice();
+
+    group.bench_function("Hrle", |b| b.iter(|| HrleVec::from(black_box(slice))));
+    group.bench_function("Rle", |b| b.iter(|| RleVec::from(black_box(slice))));
+    group.bench_function("Vec", |b| b.iter(|| Vec::from(black_box(slice))));
+
+    group.finish();
+}
+
+fn create_1000_runs_of_10_values_from_iter(c: &mut Criterion) {
+    let mut group = c.benchmark_group("1,000 runs of 10 values from iter");
+
+    let zeros = repeat_n(0, 10);
+    let ones = repeat_n(1, 10);
+    let iter = repeat_n(zeros.chain(ones), 10_000).flatten();
+
+    group.bench_function("Hrle", |b| {
+        b.iter(|| HrleVec::from_iter(black_box(iter.clone())))
+    });
+    group.bench_function("Rle", |b| {
+        b.iter(|| RleVec::from_iter(black_box(iter.clone())))
+    });
+    group.bench_function("Vec", |b| {
+        b.iter(|| Vec::from_iter(black_box(iter.clone())))
+    });
+
+    group.finish();
+}
+
+fn create_1000_runs_of_10_alternating_values_from_slice(c: &mut Criterion) {
+    let mut group = c.benchmark_group("1,000 runs of 10 alternating values from slice");
+
+    let vec = Vec::from_iter([0, 1].iter().cycle().take(10_000));
+    let slice = vec.as_slice();
+
+    group.bench_function("Hrle", |b| b.iter(|| HrleVec::from(black_box(slice))));
+    group.bench_function("Rle", |b| b.iter(|| RleVec::from(black_box(slice))));
+    group.bench_function("Vec", |b| b.iter(|| Vec::from(black_box(slice))));
+
+    group.finish();
+}
+
+fn create_1000_runs_of_10_alternating_values_from_iter(c: &mut Criterion) {
+    let mut group = c.benchmark_group("1,000 runs of 10 alternating values from iter");
+
+    group.bench_function("Hrle", |b| {
+        b.iter(|| HrleVec::from_iter([0, 1].iter().cycle().take(10_000)))
+    });
+    group.bench_function("Rle", |b| {
+        b.iter(|| RleVec::from_iter([0, 1].iter().cycle().take(10_000)))
+    });
+    group.bench_function("Vec", |b| {
+        b.iter(|| Vec::from_iter([0, 1].iter().cycle().take(10_000)))
     });
 
     group.finish();
@@ -30,7 +133,13 @@ fn bench_10_000_unique_values_from_iter(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    bench_10_000_unique_values_from_slice,
-    bench_10_000_unique_values_from_iter
+    create_10_000_unique_values_from_slice,
+    create_10_000_unique_values_from_iter,
+    create_10_000_equal_values_from_slice,
+    create_10_000_equal_values_from_iter,
+    create_1000_runs_of_10_values_from_slice,
+    create_1000_runs_of_10_values_from_iter,
+    create_1000_runs_of_10_alternating_values_from_slice,
+    create_1000_runs_of_10_alternating_values_from_iter,
 );
 criterion_main!(benches);
